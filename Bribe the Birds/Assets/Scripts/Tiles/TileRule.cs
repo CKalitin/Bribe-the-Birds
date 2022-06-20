40f7aa822833b28c51bf192d09935cf864ed1f60
CKalitin <Christopher.Kalitin@gmail.com>
2022-06-19 20:43:38 -0700

    0.0.1 - Custom Inspector for Tile Rules
    Created a custom inspector to easily and readably configure Tile Rules.

--- Bribe the Birds/Assets/Scripts/Tiles/TileRule.cs
+++ Bribe the Birds/Assets/Scripts/Tiles/TileRule.cs
@@ -11,19 +11,19 @@ public class TileRule : ScriptableObject {
     [Tooltip("This is the list of tiles needed to use the updatedTilePrefab.\n" +
         "They should be in order from the bottom y first, then moving left, like the GetAdjacentTiles function.\n" +
         "Leave Blank for no tile required.")]
-    [SerializeField] private string[] requiredAdjacentTileIDs;
+    [SerializeField] private string[] requiredAdjacentTileIds = new string[6];
 
     public string IntendedTileId { get => intendedTileId; set => intendedTileId = value; }
     public GameObject UpdatedTilePrefab { get => updatedTilePrefab; set => updatedTilePrefab = value; }
-    public string[] AdjacentTiles { get => requiredAdjacentTileIDs; set => requiredAdjacentTileIDs = value; }
+    public string[] RequiredAdjacentTiles { get => requiredAdjacentTileIds; set => requiredAdjacentTileIds = value; }
 
     // Return true is updatedTilePrefab should be used
     public bool CheckTileRule(List<Vector2Int> _adjacentTileLocs) {
         // Output is false by default so find a tile that is correct and return true
         for (int i = 0; i < _adjacentTileLocs.Count; i++) {
-            if (requiredAdjacentTileIDs[i] == "") continue; // If there is no TileRule for specified tile
+            if (requiredAdjacentTileIds[i] == "") continue; // If there is no TileRule for specified tile
             if (!TileManagement.instance.GetTile(_adjacentTileLocs[i]).Spawned) continue; // If the tile is not spawned (there is no tile at the location)
-            if (TileManagement.instance.GetTile(_adjacentTileLocs[i]).TileId != requiredAdjacentTileIDs[i]) continue; // If tile ID is correct
+            if (TileManagement.instance.GetTile(_adjacentTileLocs[i]).TileId != requiredAdjacentTileIds[i]) continue; // If tile ID is correct
 
             return true;
         }
